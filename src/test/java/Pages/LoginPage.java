6017fede04bd7ec95e57d739f8776b766f901046
nmatushevska <n.matushevska@gmail.com>
2021-02-01 21:07:07 +0300

    changes requested except for login page before tests

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,22 +1,19 @@
 package Pages;
 
-import Utils.PropertyReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.Properties;
 
 public class LoginPage extends BasePage {
-    private final Properties properties = PropertyReader.readFile();
-    private final String email = properties.getProperty("email");
-    private final String password = properties.getProperty("password");
-    private final String wrongPassword = properties.getProperty("wrongPassword");
+    private final String email = System.getProperty("email");
+    private final String password = System.getProperty("password");
+    private final String wrongPassword = System.getProperty("wrongPassword");
 
     @FindBy(xpath = "//a[contains (text(),'Я согласен')]")
     WebElement acceptCookiesButton;
-    @FindBy(xpath = "//div[@class='mh-user__text']")
+    @FindBy(xpath = "//a[@class='m-header__user']")
     WebElement accountButton;
     @FindBy(xpath = "//a[text()=' Войти ']")
     WebElement signInButton;
@@ -35,31 +32,39 @@ public class LoginPage extends BasePage {
         super(driver);
         PageFactory.initElements(driver, this);
     }
-    private void acceptCookies(){
-        acceptCookiesButton.click();
+
+    public WebElement getLogInErrorMessage() {
+        return logInErrorMessage;
     }
-    private void openLoginDialog(){
+
+    private void acceptCookies() {
+        if (accountButton.isDisplayed()) {
+            acceptCookiesButton.click();
+        }
+    }
+
+    private void openLoginDialog() {
         acceptCookies();
         actions.moveToElement(accountButton).perform();
         actions.moveToElement(signInButton).click().perform();
         logger.info("Login dialog opened");
     }
-    public void successfulLogin(){
+
+    public void successfulLogin() {
         openLoginDialog();
         emailInputField.sendKeys(email);
         passwordInputField.sendKeys(password);
         logInButton.click();
     }
-    public void failedLogin(){
+
+    public void failedLogin() {
         openLoginDialog();
         emailInputField.sendKeys(email);
         passwordInputField.sendKeys(wrongPassword);
         logInButton.click();
     }
-    public String userName() {
+
+    public String getUserName() {
         return accountName.getAttribute("title");
-    }
-    public WebElement logInErrorMessage() {
-        return logInErrorMessage;
     }
 }

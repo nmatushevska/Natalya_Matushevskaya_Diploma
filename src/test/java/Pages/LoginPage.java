67f109b56f836c193756a4aa566d5eb388288ccf
nmatushevska <n.matushevska@gmail.com>
2021-02-07 19:05:56 +0300

    Added logging with config for log4j2 using Lombok; added TestListener with overriden methods

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import Utils.PropertyReader;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.Properties;
 
-
+@Log4j2
 public class LoginPage extends BasePage {
     private final Properties properties = PropertyReader.readFile();
     private final String email = properties.getProperty("email");
@@ -17,18 +19,26 @@ public class LoginPage extends BasePage {
 
     @FindBy(xpath = "//a[contains (text(),'Я согласен')]")
     WebElement acceptCookiesButton;
+
     @FindBy(xpath = "//a[@class='m-header__user']")
     WebElement accountButton;
+
     @FindBy(xpath = "//a[text()=' Войти ']")
     WebElement signInButton;
+
     @FindBy(id = "email")
     WebElement emailInputField;
+
     @FindBy(id = "password")
     WebElement passwordInputField;
+
     @FindBy(xpath = "//button[text()='Вход']")
     WebElement logInButton;
+
+    @Getter
     @FindBy(xpath = "//p[text()='Введен неверный логин или пароль']")
     WebElement logInErrorMessage;
+
     @FindBy(xpath = "//div[@class='mh-user__text']")
     WebElement accountName;
 
@@ -37,13 +47,10 @@ public class LoginPage extends BasePage {
         PageFactory.initElements(driver, this);
     }
 
-    public WebElement getLogInErrorMessage() {
-        return logInErrorMessage;
-    }
-
     private void acceptCookies() {
         if (accountButton.isDisplayed()) {
             acceptCookiesButton.click();
+            log.info("Cookies accepted");
         }
     }
 
@@ -51,21 +58,27 @@ public class LoginPage extends BasePage {
         acceptCookies();
         actions.moveToElement(accountButton).perform();
         actions.moveToElement(signInButton).click().perform();
-        logger.info("Login dialog opened");
+        log.info("Login dialog opened");
     }
 
     public void successfulLogin() {
         openLoginDialog();
         emailInputField.sendKeys(email);
+        log.info("Email entered");
         passwordInputField.sendKeys(password);
+        log.info("Password entered");
         logInButton.click();
+        log.info("user logged in");
     }
 
     public void failedLogin() {
         openLoginDialog();
         emailInputField.sendKeys(email);
+        log.info("Email entered");
         passwordInputField.sendKeys(wrongPassword);
+        log.info("Wrong password entered");
         logInButton.click();
+        log.info("Login attempt with wrong password");
     }
 
     public String getUserName() {
---
+++ src/test/java/Utils/TestListener.java
@@ -0,0 +1,43 @@
+package Utils;
+
+import io.qameta.allure.Attachment;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.testng.IReporter;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+
+@Log4j2
+public class TestListener implements ITestListener, IReporter {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        log.info("Started test " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        log.info("Test " + result.getName() + " is performed successfully");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        log.info("Test " + result.getName() + " is skipped");
+    }
+
+    @Attachment(value = "Last screen state", type = "image/png")
+    public void onTestFailure(ITestResult result) {
+        log.info("Test " + result.getName() + " is failed");
+        WebDriver driver = (WebDriver) result.getTestContext().getAttribute("webDriver");
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(scrFile, new File("target/" + result.getName() + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
